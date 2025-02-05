3ee222e2d119ba8e10d83900dd81cc8ea0049cc4
Adwait S Purao <adwait.purao@spit.ac.in>
2025-02-05 18:13:34 +0530

    Added file paths of all notes

--- src/config/nameMapping.js
+++ src/config/nameMapping.js
@@ -1,24 +1,213 @@
 // config/nameMapping.js
 export const NAME_MAPPINGS = {
-    "Operating-Systems--OS--": "os",
-    "NOTES/Chapterwise Handwritten Notes": "notes",
-    "EXPERIMENTS/PDFS": "experiments",
-    "BOOKS": "books",
-    "QuestionPapers": "papers"
-  };
+  // Semester - 1
+  // C Programming
+  "C-programming--Problem-Solving-through-Imperative-Programming-Lab--PSIPL--": "C Programming(PSIPL)",
+  "PROGRAMS" : "Experiments",
+  "NOTES/Chapterwise Handwritten Notes": "Notes",
+
+  // DSM
+  "Digital-System-and-Microprocessors--DSM--" : "Digital Systems and Microprocessors(DSM)",
+  "DSM EXPERMENTS" : "DSM Experiments",
+
+  // Semester - 2
+  // Java
+  "Object-Oriented-Programming-with-JAVA" : "OOPS in Java",
+
+  // Semester - 3
+  // Probability and Statistics Notes
+  "Complete-Python-3-Bootcamp-Zero-To-Hero-" : "Probability and Statistics",
+  "NOTES/Probability & Statistics" : "PS Notes",
+
+  // Computer Organization and Architecture
+  "Computer-Organization-and-Architecture--COA--" : "Computer Organization and Architecture(COA)",
+
+  // Database Management Systems (DBMS)
+  "Database-Management-Systems--DBMS--" : "Database Management System(DBMS)",
+  "NOTES/DBMS AND SQL NOTES" : "DBMS and SQL Notes",
+
+  // Data Structures and Competitive Programming
+  "Data-Structures-and-Competitive-Programming--DS-CP--" : "Data Structures and Competitive Programming(DS-CP)",
+  "ALL_PROGRAMS" : "DS Experiments",
+
+  // Discrete Structures and Graph Theory
+  "Discrete-Structures-and-Graph-Theory--DSGT--" : "Discrete Structures and Graph Theory(DSGT)",
+
+  // Semester - 4
+  // Linear Algebra
+  "Linear-Algebra--LA--" : "Linear Algebra(LA)",
+  "QuestionPapers": "Question Papers",
+
+  // Design and Analysis of Algorithms
+  "Design-And-Analysis-Of-Algorithms--DAA--" : "Design And Analysis Of Algorithms(DAA)",
+  "QuestionPapers/Finishing_Touch" : "Solutions to Previous Year's Questions",
+
+  // Computer Communication and Networks
+  "Computer-Communication-and-Networks--CCN--" : "Computer Communication and Networks(CCN)",
+  "NOTES/Complete Notes in one PDF" : "Complete Notes",
+
+  // Operating Systems
+  "Operating-Systems--OS--": "Operating Systems(OS)",
+
+  // Semester - 5
+  // Theory of Computation
+  "Theory-of-Computation--TOC--" : "Theory of Computation(TOC)",
+  "NOTES/Notes along with Solutions to PYQ's" : "TOC Notes",
+
+  // Software Engineering
+  "Software-Engineering--SE--" : "Software Engineering(SE)",
+  "NOTES/My Special Notes" : "My Notes",
+  "NOTES/Solution to PYQs" : "Previous Year's Papers Solution",
+
+  // Artificial Intelligence and Machine Learning
+  "Artificial-Intelligence-and-Machine-Learning-College--AIML--" : "Artificial Intelligence and Machine Learning(AIML)",
+  "NOTES/Solutions to PYQs" : "PYQ Solutions",
+
+  // Distributed Computing
+  "Distributed-Computing--DC--" : "Distributed Computing(DC)",
+  "Question Papers" : "Previous Year's Papers",
+
+  // Cloud and Information Technology
+  "Cloud-and-Information-Technology--CITL--" : "Cloud and Information Technology(CITL)",
+
+  // Semester - 6
+  // System Programming and Compiler Construction
+  "System-Programming-and-Compiler-Construction---SPCC--" : "System Programming and Compiler Construction(SPCC)",
+
+  // Fundamentals of Signal and Image Processing
+  "Fundamentals-of-Signal-and-Image-Processing--FOSIP--" : "Fundamentals of Signal and Image Processing(FOSIP)",
+
+  // Blockchain Technology
+  "Blockchain-Technology---BCT--" : "Blockchain Technology(BCT)",
+
+  // Ethical Hacking
+  "Ethical-Hacking--EH--" : "Ethical Hacking(EH)",
+
+  // Big Data Analytics
+  "Big-Data-Analytics--BDA--" : "Big Data Analytics(BDA)",
+
+  // Semester - 7
+  // Cryptography and System Security
+  "Cryptography-and-System-Security--CSS--" : "Cryptography and System Security(CSS)",
+
+  // Artificial Intelligence for Healthcare Analytics
+  "Artificial-Intelligence-for-Healthcare-Analytics--AIH--" : "Artificial Intelligence for Healthcare Analytics(AIH)",
+
+  // Advanced Data Visualization
+  "Advanced-Data-Visualization--ADV--" : "Advanced Data Visualization(ADV)",
+
+  // Consumer Electronics
+  "Consumer-Electronics--CE--" : "Consumer Electronics(CE)",
+  "NOTES/Lecture Notes" : "Lecture Notes",
+
+};
+
+export const REVERSE_NAME_MAPPINGS = {
+  // Semester - 1
+  // C Programming
+  "C Programming(PSIPL)" : "C-programming--Problem-Solving-through-Imperative-Programming-Lab--PSIPL--",
+  "Experiments" : "PROGRAMS",
+  "Notes": "NOTES/Chapterwise Handwritten Notes",
+
+  // DSM
+  "Digital Systems and Microprocessors(DSM)": "Digital-System-and-Microprocessors--DSM--",
+  "DSM Experiments" : "DSM EXPERMENTS",
+
+  // Semester - 2
+  // Java
+  "OOPS in Java" : "Object-Oriented-Programming-with-JAVA",
+
+  // Semester - 3
+  // Probability and Statistics Notes
+  "Probability and Statistics" : "Complete-Python-3-Bootcamp-Zero-To-Hero-",
+  "PS Notes" : "NOTES/Probability & Statistics",
+
+  // Computer Organization and Architecture
+  "Computer Organization and Architecture(COA)" : "Computer-Organization-and-Architecture--COA--",
+
+  // Database Management Systems (DBMS)
+  "Database Management System(DBMS)" : "Database-Management-Systems--DBMS--",
+  "DBMS and SQL Notes" : "NOTES/DBMS AND SQL NOTES",
+
+  // Data Structures and Competitive Programming
+  "Data Structures and Competitive Programming(DS-CP)" : "Data-Structures-and-Competitive-Programming--DS-CP--",
+  "DS Experiments" : "ALL_PROGRAMS",
+
+  // Discrete Structures and Graph Theory
+  "Discrete Structures and Graph Theory(DSGT)" : "Discrete-Structures-and-Graph-Theory--DSGT--",
+
+  // Semester - 4
+  // Linear Alegebra
+  "Linear Algebra(LA)" : "Linear-Algebra--LA--",
+  "Question Papers": "QuestionPapers",
+
+  // Design and Analysis of Algorithms
+  "Design And Analysis Of Algorithms(DAA)" : "Design-And-Analysis-Of-Algorithms--DAA--",
+  "Solutions to Previous Year's Questions" : "QuestionPapers/Finishing_Touch",
+
+  // Computer Communication and Networks
+  "Computer Communication and Networks(CCN)" : "Computer-Communication-and-Networks--CCN--",
+  "Complete Notes" : "NOTES/Complete Notes in one PDF",
   
-  export const REVERSE_NAME_MAPPINGS = {
-    "os": "Operating-Systems--OS--",
-    "notes": "NOTES/Chapterwise Handwritten Notes",
-    "experiments": "EXPERIMENTS/PDFS",
-    "books": "BOOKS",
-    "papers": "QuestionPapers"
-  };
+  // Operating Systems
+  "Operating Systems(OS)": "Operating-Systems--OS--",
+
+  // Semester - 5
+  // Theory of Computation
+  "Theory of Computation(TOC)" : "Theory-of-Computation--TOC--",
+  "TOC Notes" : "NOTES/Notes along with Solutions to PYQ's",
+
+  // Software Engineering
+  "Software Engineering(SE)" : "Software-Engineering--SE--",
+  "My Notes" : "NOTES/My Special Notes",
+  "Previous Year's Papers Solution" : "NOTES/Solution to PYQs",
+
+  // Artificial Intelligence and Machine Learning
+  "Artificial Intelligence and Machine Learning(AIML)" : "Artificial-Intelligence-and-Machine-Learning-College--AIML--",
+  "PYQ Solutions" : "NOTES/Solutions to PYQs",
+
+  // Distributed Computing
+  "Distributed Computing(DC)" : "Distributed-Computing--DC--",
+  "Previous Year's Papers" : "Question Papers",
+
+  // Cloud and Information Technology
+  "Cloud and Information Technology(CITL)" : "Cloud-and-Information-Technology--CITL--",
   
-  export const getSimpleName = (actualName) => {
-    return NAME_MAPPINGS[actualName] || actualName;
-  };
+  // Semester - 6
+  // System Programming and Compiler Construction
+  "System Programming and Compiler Construction(SPCC)" : "System-Programming-and-Compiler-Construction---SPCC--",
+
+  // Fundamentals of Signal and Image Processing
+  "Fundamentals of Signal and Image Processing(FOSIP)" : "Fundamentals-of-Signal-and-Image-Processing--FOSIP--",
   
-  export const getActualName = (simpleName) => {
-    return REVERSE_NAME_MAPPINGS[simpleName] || simpleName;
-  };
+  // Blockchain Technology
+  "Blockchain Technology(BCT)" : "Blockchain-Technology---BCT--",
+
+  // Ethical Hacking
+  "Ethical Hacking(EH)" : "Ethical-Hacking--EH--",
+
+  // Big Data Analytics
+  "Big Data Analytics(BDA)" : "Big-Data-Analytics--BDA--",
+  
+  // Semester - 7
+  // Cryptography and System Security
+  "Cryptography and System Security(CSS)" : "Cryptography-and-System-Security--CSS--",
+
+  // Artificial Intelligence for Healthcare Analytics
+  "Artificial Intelligence for Healthcare Analytics(AIH)" : "Artificial-Intelligence-for-Healthcare-Analytics--AIH--",
+
+  // Advanced Data Visualization
+  "Advanced Data Visualization(ADV)" : "Advanced-Data-Visualization--ADV--",
+
+  // Consumer Electronics
+  "Consumer Electronics(CE)" : "Consumer-Electronics--CE--",
+  "Lecture Notes" : "NOTES/Lecture Notes",
+};
+
+export const getSimpleName = (actualName) => {
+  return NAME_MAPPINGS[actualName] || actualName;
+};
+
+export const getActualName = (simpleName) => {
+  return REVERSE_NAME_MAPPINGS[simpleName] || simpleName;
+};
--- src/config/structure.js
+++ src/config/structure.js
@@ -1,10 +1,121 @@
 // src/config/structure.js
 export const FOLDER_STRUCTURE = {
-    "Operating-Systems--OS--": {
-      directories: ["NOTES/Chapterwise Handwritten Notes", "EXPERIMENTS/PDFS", "BOOKS", "QuestionPapers","Images","NOTES/Class Notes"]
-    },
-    // "Operating-Systems": {
-    //   directories: ["notes", "experiments", "codes", "previous-papers"]
-    // }
-    // Add other subjects as needed
-  };
+  // "" : {
+  //   directories : ["",""]
+  // },
+  // Semester - 1
+  "C-programming--Problem-Solving-through-Imperative-Programming-Lab--PSIPL--": {
+    directories: ["NOTES/Chapterwise Handwritten Notes","PROGRAMS"]
+  },
+  "Digital-System-and-Microprocessors--DSM--" : {
+    directories : ["DSM Notes","DSM EXPERMENTS"]
+  },
+
+  // Semester - 2
+  "Object-Oriented-Programming-with-JAVA" : {
+    directories : ["NOTES/Chapterwise Handwritten Notes","PROGRAMS"]
+  },
+
+  // Semester - 3
+  //Probability and Statistics Notes
+  "Complete-Python-3-Bootcamp-Zero-To-Hero-" : {
+    directories : ["NOTES/Probability & Statistics"]
+  },
+  "Computer-Organization-and-Architecture--COA--" : {
+    directories : ["NOTES","EXPERIMENTS"]
+  },
+  "Database-Management-Systems--DBMS--" : {
+    directories : ["NOTES/DBMS AND SQL NOTES","EXPERIMENTS"]
+  },
+  "Data-Structures-and-Competitive-Programming--DS-CP--" : {
+    directories : ["NOTES/Chapterwise Handwritten Notes","ALL_PROGRAMS","CP NOTES"]
+  },
+  "Discrete-Structures-and-Graph-Theory--DSGT--" : {
+    directories : ["NOTES"]
+  },
+
+  // Semester - 4
+  "Linear-Algebra--LA--" : {
+    directories : ["NOTES","QuestionPapers"]
+  },
+  "Design-And-Analysis-Of-Algorithms--DAA--" : {
+    directories : ["NOTES/Chapterwise Handwritten Notes","QuestionPapers","QuestionPapers/Finishing_Touch"]
+  },
+  "Computer-Communication-and-Networks--CCN--" : {
+    directories : ["NOTES/Complete Notes in one PDF","QuestionPapers","QuestionPapers/Finishing_Touch"]
+  },
+  "Operating-Systems--OS--": {
+    directories: ["NOTES/Chapterwise Handwritten Notes", "QuestionPapers","QuestionPapers/Finishing_Touch"]
+  },
+
+  // Semester - 5
+  "Theory-of-Computation--TOC--" : {
+    directories : ["NOTES/Notes along with Solutions to PYQ's","Question Papers"]
+  },
+  "Software-Engineering--SE--" : {
+    directories : ["NOTES/My Special Notes","NOTES/Solution to PYQs","Question Papers"]
+  },
+  "Artificial-Intelligence-and-Machine-Learning-College--AIML--" : {
+    directories : ["NOTES/My Special Notes","NOTES/Solutions to PYQs","Question Papers"]
+  },
+  "Distributed-Computing--DC--" : {
+    directories : ["NOTES/Solution to PYQs","Question Papers"]
+  },
+  "Cloud-and-Information-Technology--CITL--" : {
+    directories : ["EXPERIMENTS"]
+  },
+
+  // Semester - 6
+  "System-Programming-and-Compiler-Construction---SPCC--" : {
+    directories : ["NOTES/My Special Notes","NOTES/Solutions to PYQs","Question Papers"]
+  },
+  "Fundamentals-of-Signal-and-Image-Processing--FOSIP--" : {
+    directories : ["NOTES/My Special Notes","NOTES/Solution to PYQs","Question Papers"]
+  },
+  "Blockchain-Technology---BCT--" : {
+    directories : ["NOTES/Chapterwise Notes","Question Papers","NOTES/Complete Notes in one PDF"]
+  },
+  "Ethical-Hacking--EH--" : {
+    directories : ["NOTES/Complete Notes in one PDF","NOTES/Solution to PYQs","Question Papers"]
+  },
+  "Big-Data-Analytics--BDA--" : {
+    directories : ["NOTES/My Special Notes","NOTES/Solution to PYQs","Question Papers"]
+  },
+
+  // Semester - 7
+  "Cryptography-and-System-Security--CSS--" : {
+    directories : ["NOTES/My Special Notes","Question Papers","NOTES/Solution to PYQs"]
+  },
+  "Artificial-Intelligence-for-Healthcare-Analytics--AIH--" : {
+    directories : ["NOTES/My Special Notes"]
+  },
+  "Advanced-Data-Visualization--ADV--" : {
+    directories : ["NOTES/My Special Notes","NOTES/Solution to PYQs","QuestionPapers"]
+  },
+  "Consumer-Electronics--CE--" : {
+    directories : ["NOTES/Lecture Notes","NOTES/Solution to PYQs","Question Papers",]
+  },
+  "Environment-and-Development" : {
+    directories : ["NOTES","ASSIGNMENTS"]
+  },
+
+  // Semester - 8 (Interview Preparation Resources)
+  "Placement-Aptitude-Preparation" : {
+    directories : ["Aptitude Questions"]
+  },
+  "Web-Development" : {
+    directories : ["HTML and CSS","JAVASCRIPT"]
+  },
+  // "Data-Structures-and-Competitive-Programming--DS-CP--" : {
+  //   directories : ["CP NOTES","ESE_QUESTIONS"]
+  // },
+  "Github-Tutorial" : {
+    directories : ["Comprehensive List of Commands","Cheatsheet"]
+  },
+  "Design-Thinking-AND-Innovation-and-Creativity" : {
+    directories : ["Innovation and Creativity","UI-UX-Redesign-Case-Study"]
+  },
+  "Electronics-Notes" : {
+    directories : ["HSC NOTES"]
+  },
+};
