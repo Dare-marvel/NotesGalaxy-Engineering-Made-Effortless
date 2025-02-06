70d124634821e8d0a5bf6fa2a6b206d55f0e7dac
Adwait S Purao <adwait.purao@spit.ac.in>
2025-02-06 16:15:49 +0530

    Resolving vulnerabilities of vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    cors: {
+      origin: "https://notes-galaxy.vercel.app", // Allow only your deployed frontend
+    },
+    allowedHosts: ["notes-galaxy.vercel.app"], // Explicitly allow your frontend domain
+    origin: "https://notes-galaxy.vercel.app", // Secure WebSocket connections
+  }
 })
