2e1d8bceaa9c239ce2ba0c4aa815ce7666afdf05
Adwait S Purao <adwait.purao@spit.ac.in>
2025-02-06 21:49:25 +0530

    Added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   server: {
     cors: {
@@ -10,6 +9,5 @@ export default defineConfig({
     },
     allowedHosts: ["notes-galaxy.vercel.app"], // Explicitly allow your frontend domain
     origin: "https://notes-galaxy.vercel.app", // Secure WebSocket connections
-    historyApiFallback: true,
   }
 })
