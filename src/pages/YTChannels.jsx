ad016aef7c1992c831fa7132f2803cebe6899923
Adwait S Purao <adwait.purao@spit.ac.in>
2025-05-29 23:32:44 +0530

    Added yt channels link for some more subjects

--- src/pages/YTChannels.jsx
+++ src/pages/YTChannels.jsx
@@ -38,6 +38,20 @@ const subjectsData = {
         { name: "Digital Electronics for GATE by Tutorials Point", url: "https://youtube.com/playlist?list=PLWPirh4EWFpHk70zwYoHu87uVsCC8E2S-&feature=shared" },
         { name: "Engineering Funda", url: "https://youtube.com/playlist?list=PLgwJf8NK-2e7nYSG31YWEUfwgAp2uIOBY&feature=shared" }
       ]
+    },
+    {
+      name: "Basic Electrical Engineering (BEE)",
+      channels: [
+        { name: "Basic Electrical Engineering by Umesh Dhande", url: "https://youtube.com/playlist?list=PL9RcWoqXmzaLTYUdnzKhF4bYug3GjGcEc&feature=shared" },
+        { name: "Basic Electrical Engineering - Perfect Computer Engineer", url: "https://youtube.com/playlist?list=PLPIwNooIb9vhiZRRq1fEWXvSLz7VMeqSh&feature=shared" },
+        { name: "Basic Electrical Engineering by Gautam Varde", url: "https://youtube.com/playlist?list=PL3qvHcrYGy1v2kJX4SSsurE3_GdVe0ZD5&feature=shared" }
+      ]
+    },
+    {
+      name: "Engineering Graphics (EG)",
+      channels: [
+        { name: "Tikle's Academy", url: "https://youtube.com/playlist?list=PLDN15nk5uLiD3MEUiqsYPnZOHcVu7um6_&feature=shared" },
+      ]
     }
   ],
   "Semester 2": [
